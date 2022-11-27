5d7b4f3d9b477a1649890d3966cdcccc64e59e40
Jade Park <dev.jadepark@gmail.com>
2022-11-27 10:15:00 +0900

    batch config test

--- src/main/java/io/springbatch/springbatchlecture/config/JobParameterConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/config/JobParameterConfiguration.java
@@ -20,48 +20,48 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class JobParameterConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job job() {
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        JobParameters jobParameters = stepContribution.getStepExecution().getJobExecution().getJobParameters();
-                        jobParameters.getString("name");
-                        jobParameters.getLong("seq");
-                        jobParameters.getDate("date");
-                        jobParameters.getDouble("age");
-                        //ChunkContext: attributes=[], complete=false, stepContext=SynchronizedAttributeAccessor: [], stepExecutionContext={batch.taskletType=io.springbatch.springbatchlecture.config.JobParameterConfiguration$1, batch.stepType=org.springframework.batch.core.step.tasklet.TaskletStep}, jobExecutionContext={}, jobParameters={date=Tue Nov 22 00:14:22 KST 2022, name=user1, seq=2, age=16.5}
-
-                        Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
-
-                        return RepeatStatus.FINISHED;   //한번실행
-
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        return RepeatStatus.FINISHED;   //한번실행
-                    }
-                })
-                .build();
-    }
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job job() {
+//        return jobBuilderFactory.get("job")
+//                .start(step1())
+//                .next(step2())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        JobParameters jobParameters = stepContribution.getStepExecution().getJobExecution().getJobParameters();
+//                        jobParameters.getString("name");
+//                        jobParameters.getLong("seq");
+//                        jobParameters.getDate("date");
+//                        jobParameters.getDouble("age");
+//                        //ChunkContext: attributes=[], complete=false, stepContext=SynchronizedAttributeAccessor: [], stepExecutionContext={batch.taskletType=io.springbatch.springbatchlecture.config.JobParameterConfiguration$1, batch.stepType=org.springframework.batch.core.step.tasklet.TaskletStep}, jobExecutionContext={}, jobParameters={date=Tue Nov 22 00:14:22 KST 2022, name=user1, seq=2, age=16.5}
+//
+//                        Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+//
+//                        return RepeatStatus.FINISHED;   //한번실행
+//
+//                    }
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        return RepeatStatus.FINISHED;   //한번실행
+//                    }
+//                })
+//                .build();
+//    }
 }
